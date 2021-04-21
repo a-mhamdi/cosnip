452057bd329c0dac3533925f7eaceb0354619f6a
a-mhamdi <a_mhamdi@outlook.com>
2021-04-22 00:10:39 +0100

    update codes

---
+++ Matlab/gui/Main.m
@@ -0,0 +1,106 @@
+function varargout = Main(varargin)
+% MAIN MATLAB code for Main.fig
+%      MAIN, by itself, creates a new MAIN or raises the existing
+%      singleton*.
+%
+%      H = MAIN returns the handle to a new MAIN or the handle to
+%      the existing singleton*.
+%
+%      MAIN('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MAIN.M with the given input arguments.
+%
+%      MAIN('Property','Value',...) creates a new MAIN or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Main_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Main_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Main
+
+% Last Modified by GUIDE v2.5 09-Apr-2016 17:48:34
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Main_OpeningFcn, ...
+                   'gui_OutputFcn',  @Main_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Main is made visible.
+function Main_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Main (see VARARGIN)
+
+% Choose default command line output for Main
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Main wait for user response (see UIRESUME)
+% uiwait(handles.TagMain);
+if strcmp(get(hObject,'Visible'),'off')
+    load EspritLogo;
+    axes(handles.TagLogo);
+    image (cdata);
+    colormap(map);
+    grid on;
+    axis off;
+end
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Main_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+selection = questdlg(['Close ' get(handles.TagMain,'Name') '?'],...
+                     ['Close ' get(handles.TagMain,'Name')],...
+                     'Yes','No','Yes');
+if strcmp(selection,'No')
+    return;
+else
+    delete(handles.TagMain);
+end
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+InterfaceI;
+InterfaceII;
+InterfaceIII;
+delete(handles.TagMain);
