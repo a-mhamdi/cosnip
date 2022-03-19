bca1c496e18021f0f0969219788c80817ceafb57
Abdelbacet Mhamdi <42786918+a-mhamdi@users.noreply.github.com>
2022-03-19 11:07:10 +0100

    fix the step plot response

--- Julia/sys-ctrl.jl
+++ Julia/sys-ctrl.jl
@@ -3,8 +3,12 @@ using ControlSystems
 s = tf("s")
 sys = s/(s+1)
 t = 2:.5:10
-stepplot(sys,t)
+# PLOT THE STEP RESPONSE
+plot(step(sys,t))
+# PLOT BODE FREQUENCY RESPONSE
 bodeplot(sys)
+# PLOT NYQUIST FREQUENCY RESPONSE
 nyquistplot(sys)
+# PLOT NICHOLS FREQUENCY RESPONSE
 nicholsplot(sys)
 
