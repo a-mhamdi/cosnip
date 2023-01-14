acd344f5741527686dfad2954d465f740046792d
a-mhamdi <a_mhamdi@outlook.com>
2023-01-14 23:24:51 +0100

    fix arch of ann

--- Julia/nnet/ann.jl
+++ Julia/nnet/ann.jl
@@ -1,61 +1,92 @@
+#####################################################
+#= BINARY CLASSIFIER USING ANN _(CHURN MODELLING)_ =#
+#####################################################
+
+## Import the required librairies
 using CSV, DataFrames
 using MLJ
+using Flux
 
+## Hyperparameters tuning
+η, epochs, batchsize = .001, 1_000, 32
+
+## Load data for csv file
 df = CSV.read("./datasets/Churn_Modelling.csv", DataFrame)
-schema(df) # dropmissing!(df)
 
-first(df, 5)
-names(df)
-
+# Choose the target vector `y`
 ydf = select(df, :Exited)
 y = ydf.Exited
 
+## Specify the features matrix `X`
 Xdf = select(df, Not([:RowNumber, :CustomerId, :Surname, :Exited]))
+coerce!(Xdf,
+    :Geography => Multiclass,
+    :Gender => Multiclass)
 
-names(Xdf)
-first(Xdf, 5)
+## Onehotencoding of multiclass variables
+ce = ContinuousEncoder()
+Xdf = machine(ce, Xdf) |> fit! |> MLJ.transform
 
+## Features scaling
+sc = Standardizer()
+Xdf = machine(sc, Xdf) |> fit! |> MLJ.transform
+
+## Extract only the values for `X`, i.e, rm the headers.
 n, m = size(Xdf)
-X = Array{Any, 2}(undef, (n, m));
+X = Array{Float64, 2}(undef, (n, m));
 
 for i in 1:m
     X[:, i] = Xdf[!, i];
 end
 
-countries = sort(unique(reduce(vcat, Xdf.Geography)))
-gender = sort(unique(reduce(vcat, Xdf.Gender)))
-
-using OneHotArrays
-
-b = onehotbatch(X[:, 2], countries)
-c = onehotbatch(X[:, 3], gender)
-
-using Flux
-
+## Design the architecture of the classifier, denoted hereafter by `clf`
 clf = Chain(
-    Dense( m  => 12, relu ),
-    Dense( 12 => 16, relu ),
-    Dense( 8  => 1, σ )
+            Dense( 13 => 8, relu),
+            Dense( 8  => 8, relu),
+            Dense( 8  => 4, relu),
+            Dense( 4  => 1, σ)
             )
 
+## Permute dims: ROW => features and COL => observation
 X = permutedims(X)
 y = permutedims(y)
 
-data = Flux.DataLoader((X, y), batchsize=16, shuffle=true)
-optim_state = Flux.setup(Flux.Adam(.01), clf)
+## Optimizers and data loader
+optim_state = Flux.setup(Adam(η), clf);
+loader = Flux.Data.DataLoader((X, y); batchsize=batchsize, shuffle=true);
 
+vec_loss = []
 
-losses = []
-
+### Training phase
 using ProgressMeter
-@showprogress for epoch in 1:1_000
-    for (x, y) in data 
-        loss, grad = Flux.withgradient(clf) do mdl
+@showprogress for _ in 1:epochs
+    for (X, y) in loader
+        loss, grads = Flux.withgradient(clf) do mdl
             ŷ = mdl(X);
-            Flux.crossentropy(ŷ, y);
+            Flux.logitbinarycrossentropy(ŷ, y);
         end
-        Flux.update!(optim_state, clf, grads[1])
-        push!(losses, loss)
+        Flux.update!(optim_state, clf, grads[1]); # Upd `W` and `b`
+        push!(vec_loss, loss); # Log `loss` to the vector `vec_loss`
     end
-    sleep(.01)
 end
+
+## Plot the loss vector `vec_loss`
+using Plots
+plot(vec_loss, label="Loss")
+extrema(vec_loss)
+
+## Some metrics
+ŷ = clf(X);
+ŷ = (ŷ .≥ .5);
+# y = (y .≥ .5);
+
+## Basic way to compute the accuracy
+accuracy = sum(mean( ŷ .== y))
+
+## Confusion Matrix
+cm = MLJ.ConfusionMatrix()(ŷ, y)
+TP, TN, FP, FN = cm[2, 2], cm[1, 1], cm[2, 1], cm[1, 2];
+accuracy_ = (TP+TN)/(TP+TN+FP+FN) # MLJ.accuracy(cm)
+precision_ = TP/(TP+FP) # MLJ.precision(cm)
+recall_ = TP/(TP+FN) # MLJ.recall(cm)
+f1score_ = 2/(1/precision_ + 1/recall_) # MLJ.f1score(cm)
