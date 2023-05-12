4fe1953d95328cf4acbbbc8e3e28c557dd80837b
a-mhamdi <a_mhamdi@outlook.com>
2023-05-12 18:31:48 +0100

    deploy a web service

---
+++ Python/ml/iris_ws.py
@@ -0,0 +1,42 @@
+#! /home/mhamdi/appware/virtenv/venv/bin/python3
+
+import urllib.request as request
+
+import json 
+
+data =  {
+
+        "Inputs": {
+
+                "input1":
+                {
+                    "ColumnNames": ["sepal-length", "sepal-width", "petal-length", "petal-width"],
+                    "Values": [ [ "6", "3", "4.8", "1.8" ], # 1
+                                [ "5.1", "3.5", "1.4", ".3" ], # 0
+                               ]
+                },        },
+            "GlobalParameters": {
+}
+    }
+
+body = str.encode(json.dumps(data))
+
+url = os.environ['URL']
+api_key = os.environ['API_KEY']
+
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = request.Request(url, body, headers) 
+
+try:
+    response = request.urlopen(req)
+
+    result = response.read()
+    print(result) 
+except request.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the request ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+
+    print(json.loads(error.read()))                 
