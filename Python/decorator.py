623d8b95ec9be7b340742a3e9b4f4376ebb61232
a-mhamdi <a_mhamdi@outlook.com>
2024-02-14 09:01:29 +0100

    decorator in python

---
+++ Python/decorator.py
@@ -0,0 +1,17 @@
+# Decorator function
+def greeting_decorator(func):
+    def wrapper():
+        print("Before calling the function")
+        func()
+        print("After calling the function")
+    return wrapper
+
+# Decorating the greet function
+@greeting_decorator
+def greet():
+    print("Hello, World!")
+
+# Calling the decorator function and greet = greeting_decorator(greet)
+
+# Calling the decorated function
+greet()
