4f6a9d2a513e799da00bfb28f9e60de80b98dcd1
a-mhamdi <a_mhamdi@outlook.com>
2024-04-23 14:57:34 +0100

    update web-app

--- Julia/sum_app.jl
+++ Julia/sum_app.jl
@@ -5,13 +5,13 @@ Genie.Generator.newapp("app")
 
 form = """
     <form action="/sum" method="POST">
-        <label for="num1">Number 1: </label>
-        <input type="text" id="num1" name="num1" value=" " placeholder="1st number"><br>
-        <label for="num2">Number 2: </label>
-        <input type="text" id="num2" name="num2" value=" " placeholder="2nd number"><br>
+        <label for="label1">Number 1: </label>
+        <input type="text" id="num1" name="num1" placeholder="1st number"><br>
+        <label for="label2">Number 2: </label>
+        <input type="text" id="num2" name="num2" placeholder="2nd number"><br>
         <input type="submit" value="Submit">
     </form>
-    """
+"""
 
 # Define a route to handle GET requests for the home page
 route("/") do
@@ -20,9 +20,10 @@ end
 
 # Define a route to handle POST requests and compute the sum
 route("/sum", method=POST) do
-    num1 = parse(String, params(:num1)) 
-    num2 = parse(String, params(:num2)) 
-    sum = *(num1, num2)
+    num1 = parse(Float32, params(:num1))
+    num2 = parse(Float32, params(:num2))
+    sum = +(num1, num2)
 
     "The sum of $num1 and $num2 is $sum"
 end
+
